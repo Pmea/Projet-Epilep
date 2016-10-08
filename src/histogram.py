d9c3db8adb072444d7149f8940c786ccfd352e15
pmea <kurugan04@hotmail.fr>
2016-10-08 12:44:25 +0200

    correction de deux bugs et creation de la somme spectral

--- src/histogram.py
+++ src/histogram.py
@@ -93,8 +93,15 @@ def compute_spectral_produit(m_tfSig, nfft, H):
     return v_P
 
 
-def compute_spectral_sum(m_tfsig):
-
+def compute_spectral_sum(m_tfsig, nfft, H):
+    #calcule de R
+    Rmax= int(np.floor((nfft / (2*H))))
+    #calcule du produit spectral
+    v_S= np.zeros(Rmax)
+    for i in range(0, Rmax):
+        for h in range(0,H):
+            v_S[i] = v_S[i] + np.abs(H * m_tfSig[h*i])
+    return v_S
 
 
 def compute_tfct_all_pixel(video):
@@ -112,34 +119,36 @@ def compute_tfct_all_pixel(video):
 
     # test 
     ## resample 
-    deriv_p_elev= resample_data(v_sig, 5)
+    deriv_p_elev= resample_data(deriv_p, 10)
     ## filtrage
     v_fen= np.array([0.25, 0.5, 1, 0.5, 0.25])
     deriv_p_elev= apply_conv(deriv_p_elev, v_fen)
 
 
     #calcule de la tf global
-    nfft= 2**10                                     
+    nfft= 2**10                                 
     winSize= 2**10
-    hopRatio= 1./4
+    hopRatio= 1./8 
 
     SIG= tfct.tfct(deriv_p_elev, nfft, winSize, hopRatio)           #verifier que la tfct n'a pas de bug #si elle n'en a pas voir pour meilleur fenetre (plus d'attenuation)
     nb_bin= SIG.shape[0]
-    
-    H=4    # si Rmax est superieur a nfft il y a du recouvrement donc ca ne marche pas 
+
+    H=8    # si Rmax est superieur a nfft il y a du recouvrement donc ca ne marche pas 
 
     v_max_P= np.zeros(nb_bin)
     for i in range (0, nb_bin):
         v_P=compute_spectral_produit(SIG[i,:], nfft, H)
         v_max_P[i]= np.argmax(v_P)
 
-    plt.plot(v_max_P)
-    plt.show()
 
     SIG_abs= np.abs(SIG)
     SIG_abs= np.transpose(SIG_abs)
 
     max_sig= np.argmax(SIG_abs[:nfft/2], axis=0)
+
+    plt.plot(v_max_P, c='y')
+    plt.plot(max_sig, c='r')
+    plt.show()
 
     plt.imshow(np.transpose(np.abs(SIG)), aspect="auto")
     plt.plot(max_sig, c='r')
@@ -195,27 +204,27 @@ def scale_video(video, new_width, new_height, filename_dest=None):
 
     return scale_video
 
-def main():
-    print ("Debut du test")
-    filename= "~/Projet/Projet-Epilep/video/video-test-synthesis-4.mov"
-    main_clip= load_video(filename)
-    scale_clip=scale_video(main_clip, 40, 25)#, filename+"-scale.mp4")
+#def main():
+print ("Debut du test")
+filename= "~/Projet/Projet-Epilep/video/video-test-synthesis-4.mov"
+main_clip= load_video(filename)
+scale_clip=scale_video(main_clip, 40, 25)#, filename+"-scale.mp4")
 
-    video_mtx, nb_frames, W, H= videoclip_to_matrix(scale_clip)
+video_mtx, nb_frames, W, H= videoclip_to_matrix(scale_clip)
 
-    CHD= compute_CHD(video_mtx)
-    FAD= compute_FAD(video_mtx)
+CHD= compute_CHD(video_mtx)
+FAD= compute_FAD(video_mtx)
 
-    plt.plot(CHD, label="CHD")
-    plt.plot(FAD, label="FAD")
-    plt.legend()
-    plt.show()
+plt.plot(CHD, label="CHD")
+plt.plot(FAD, label="FAD")
+plt.legend()
+plt.show()
 
-    print ("Fin du test")
+print ("Fin du test")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
 
